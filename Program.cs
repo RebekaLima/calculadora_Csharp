3171e7203d478b1cbe0aa09def31d1c35f9f2e7e
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-02-14 15:17:12 -0300

    Melhorias implementadas

--- Program.cs
+++ Program.cs
@@ -1,18 +1,72 @@
 ﻿double entrada1, entrada2;
 int resultado;
+int escolha;
 
-while (true)
-{
-    Console.WriteLine("Digite o primeiro número: ");
-    entrada1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número referente a operação que deseja realizar: ");
+Console.WriteLine("1 - Adição");
+Console.WriteLine("2 - Subtração");
+Console.WriteLine("3 - Multiplicação");
+Console.WriteLine("4 - Divisão");
+escolha = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Digite o segundo número: ");
-    entrada2 = Double.Parse(Console.ReadLine());
+switch(escolha) {
+    case 1:
+        Console.WriteLine("Digite o primeiro número: ");
+        entrada1 = Double.Parse(Console.ReadLine());
 
-    resultado = (int) (entrada1 + entrada2);
+        Console.WriteLine("Digite o segundo número: ");
+        entrada2 = Double.Parse(Console.ReadLine());
 
-    Console.Write($"O resultado da soma é: {resultado}");
-    System.Threading.Thread.Sleep(5000);
-    Console.Clear();
-}
+        resultado = (int)(entrada1 + entrada2);
+
+        Console.Write($"O resultado da soma é: {resultado}");
+        System.Threading.Thread.Sleep(5000);
+        Console.Clear();
+        break;
+
+    case 2:
+        Console.WriteLine("Digite o primeiro número: ");
+        entrada1 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        entrada2 = Double.Parse(Console.ReadLine());
+
+        resultado = (int)(entrada1 - entrada2);
+
+        Console.Write($"O resultado da soma é: {resultado}");
+        System.Threading.Thread.Sleep(5000);
+        Console.Clear();
+        break;
+
+    case 3:
+        Console.WriteLine("Digite o primeiro número: ");
+        entrada1 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        entrada2 = Double.Parse(Console.ReadLine());
+
+        resultado = (int)(entrada1 * entrada2);
+
+        Console.Write($"O resultado da soma é: {resultado}");
+        System.Threading.Thread.Sleep(5000);
+        Console.Clear();
+        break;
+
+    case 4:
+        Console.WriteLine("Digite o primeiro número: ");
+        entrada1 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        entrada2 = Double.Parse(Console.ReadLine());
+
+        resultado = (int)(entrada1 / entrada2);
+
+        Console.Write($"O resultado da soma é: {resultado}");
+        System.Threading.Thread.Sleep(5000);
+        Console.Clear();
+        break;
+    default:
+        Console.WriteLine("Número inválido :(");
+        break;
+    }
 
--- obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ece6120ceb75fb854095c19efd4e3576fe1d89f")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
