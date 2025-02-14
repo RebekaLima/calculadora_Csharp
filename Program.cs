5ece6120ceb75fb854095c19efd4e3576fe1d89f
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-02-14 15:05:22 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double entrada1, entrada2;
+int resultado;
+
+while (true)
+{
+    Console.WriteLine("Digite o primeiro número: ");
+    entrada1 = Double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número: ");
+    entrada2 = Double.Parse(Console.ReadLine());
+
+    resultado = (int) (entrada1 + entrada2);
+
+    Console.Write($"O resultado da soma é: {resultado}");
+    System.Threading.Thread.Sleep(5000);
+    Console.Clear();
+}
+
